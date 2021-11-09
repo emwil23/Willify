cd9f986d23971a8c62e88e2215bbc901966400da
Emmanuel Wilson <emmanuelwilson@Emmanuels-MacBook.local>
2021-11-09 21:54:18 +0530

    .

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { ReactComponent as IconLight } from '../assets/icon-light.svg';
 
 const AUTH_URL =
-  'https://accounts.spotify.com/authorize?client_id=82ba1125a54642d7944679a6e2a5b8d9&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state';
+  'https://accounts.spotify.com/authorize?client_id=82ba1125a54642d7944679a6e2a5b8d9&response_type=code&redirect_uri=https://emwil23.github.io/Willify&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state';
 
 export default function Login() {
   return (
